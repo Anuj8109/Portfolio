9bdd9de9f642a3eb53551df1bcc0f54e9d51620e
unknown <anujchhirolya9522@gmail.com>
2020-08-03 23:49:37 +0530

    Data problem solve

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -132,8 +132,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'portfolio', 'media')
+MEDIA_URL = '/portfolio/media/'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
